76e33b6713e78c40abb5e38d2db95ff8b38e0c86
zhanghan <zhanghan_java@163.com>
2019-01-12 16:37:52 +0800

    优化启动类

--- zh-boot/src/main/java/com/zhanghan/zhboot/ZhBootApplication.java
+++ zh-boot/src/main/java/com/zhanghan/zhboot/ZhBootApplication.java
@@ -2,19 +2,12 @@ package com.zhanghan.zhboot;
 
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@EnableAutoConfiguration(exclude = {
+@SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
-        MybatisAutoConfiguration.class
-})
-@ComponentScan({
-       "com.zhanghan.zhboot"
-})
+        MybatisAutoConfiguration.class})
 public class ZhBootApplication {
 
     public static void main(String[] args) {
