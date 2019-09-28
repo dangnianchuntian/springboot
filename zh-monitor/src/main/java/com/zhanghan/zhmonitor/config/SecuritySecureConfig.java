dcdd4d1bd9f86a4e780049acabba8a59f2425221
zhanghan <zhanghan_java@163.com>
2019-09-28 09:26:51 +0800

    集成安全和告警模块

---
+++ zh-monitor/src/main/java/com/zhanghan/zhmonitor/config/SecuritySecureConfig.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2019. zhanghan_java@163.com All Rights Reserved.
+ * 项目名称：实战SpringBoot
+ * 类名称：SecuritySecureConfig.java
+ * 创建人：张晗
+ * 联系方式：zhanghan_java@163.com
+ * 开源地址: https://github.com/dangnianchuntian/springboot
+ * 博客地址: https://zhanghan.blog.csdn.net
+ */
+
+package com.zhanghan.zhmonitor.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+
+    private final String adminContextPath;
+
+    public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(adminContextPath + "/");
+
+
+        http.authorizeRequests()
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                .logout().logoutUrl(adminContextPath + "/logout").and()
+                .httpBasic().and()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringAntMatchers(
+                        adminContextPath + "/instances",
+                        adminContextPath + "/actuator/**");
+
+        // @formatter:on
+    }
+
+}
