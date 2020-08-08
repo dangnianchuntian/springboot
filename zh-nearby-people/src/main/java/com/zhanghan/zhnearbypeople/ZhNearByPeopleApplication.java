68d3c41f320df03c6c47b471cda349973fd043d1
zhanghan <zhanghan_java@163.com>
2020-08-09 00:15:47 +0800

    add nearby people

---
+++ zh-nearby-people/src/main/java/com/zhanghan/zhnearbypeople/ZhNearByPeopleApplication.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020. zhanghan_java@163.com All Rights Reserved.
+ * 项目名称：Spring Boot实战解决高并发数据入库: Redis 缓存+MySQL 批量入库
+ * 类名称：RedisConfig.java
+ * 创建人：张晗
+ * 联系方式：zhanghan_java@163.com
+ * 开源地址: https://github.com/dangnianchuntian/springboot
+ * 博客地址: https://zhanghan.blog.csdn.net
+ */
+
+package com.zhanghan.zhnearbypeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhNearByPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhNearByPeopleApplication.class, args);
+	}
+
+}
---
+++ zh-nearby-people/src/main/java/com/zhanghan/zhnearbypeople/dto/ArticleCountDto.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020. zhanghan_java@163.com All Rights Reserved.
+ * 项目名称：Spring Boot实战解决高并发数据入库: Redis 缓存+MySQL 批量入库
+ * 类名称：RedisConfig.java
+ * 创建人：张晗
+ * 联系方式：zhanghan_java@163.com
+ * 开源地址: https://github.com/dangnianchuntian/springboot
+ * 博客地址: https://zhanghan.blog.csdn.net
+ */
+
+package com.zhanghan.zhnearbypeople.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class ArticleCountDto {
+
+    private String buNo;
+
+    private String customerId;
+
+    private String articleNo;
+
+    private Date readTime;
+
+}
